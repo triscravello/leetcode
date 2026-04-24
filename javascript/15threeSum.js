f4791941fe9ab5f13e93499c15c24f5928d3954c
Tristan Cravello <tlcravello@gmail.com>
2026-04-24 12:50:42 -0400

    Add problem 15

---
+++ javascript/15threeSum.js
@@ -0,0 +1,40 @@
+/**
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such at i != j, i!= k, and j!= k, and nums[i] + nums[j] + nums[k] == 0
+ * 
+ * Notice that the solution set must not contain duplicate triplets.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const result = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
+
+                while (left < right && nums[left] === nums[left - 1]) left++;
+                while (left < right && nums[right] === nums[right + 1]) right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
